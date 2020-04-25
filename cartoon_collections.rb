24ecdcfbf4350e4ef1d994e4cd4ca832a71658b7
Jung Lee <lee.jung.s95@gmail.com>
2020-04-25 04:26:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,5 @@ end
 
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_types.select{|i| i.include?(cheddar_cheese)}
-  else
-    false
-  end
+  cheese_types.select{|i| i.include?(cheddar_cheese)}
 end
