9fbccabbbbc5b841fa6403fda7e17f56014712cd
Jung Lee <lee.jung.s95@gmail.com>
2020-04-25 03:50:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ end
 
 def summon_captain_planet(veggies)
   array = []
-  veggies.collect do |veggies|
-    array << "#{veggies}.titleize!"
+  veggies.collect do |veggies.titleize|
+    array << "#{veggies}!"
   end
   array
 end
