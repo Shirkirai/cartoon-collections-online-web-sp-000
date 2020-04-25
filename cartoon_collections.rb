151c8819aaabf0f4a7e725da394dd721ef7674df
Jung Lee <lee.jung.s95@gmail.com>
2020-04-25 04:15:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def long_planeteer_calls(call)# code an argument here
   end# Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheddar_cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
