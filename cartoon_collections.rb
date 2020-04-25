a2d9fa902513b224c508f62a41edfb78a6446651
Jung Lee <lee.jung.s95@gmail.com>
2020-04-25 04:11:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls(call)# code an argument here
-  if call.length > 4
+  if call.any?.length > 4
     return true
   else
     return false
