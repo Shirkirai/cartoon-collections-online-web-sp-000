71ef21f98f71ff20327f7fa02656982feddc15c6
Jung Lee <lee.jung.s95@gmail.com>
2020-04-25 05:13:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if cheese_types.include?(cheddar_cheese)
-    return cheese_types.select do |cheese|
+    cheese_types.select do |cheese|
       cheese
   else
     false
